477eee327812e4850b1af7deb6199f2fb9fd041a
Your Name Here <your_email@example.com>
2013-11-27 17:48:06 +0100

    <<

--- ims/main.cpp
+++ ims/main.cpp
@@ -6,22 +6,32 @@
 //  Copyright (c) 2013 Kryštof Matěj. All rights reserved.
 //
 
-#include <iostream>
-#include <chrono>
-#include <thread>
+const double F = 1.0;
 
-int main(int argc, const char * argv[])
-{
+class Integrator;
+class Graph;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    while(1)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        for (int i = 0; i < 100; i++) {
-//            std::cout << "vojta" << std::endl;
-        }
-    }
+class Kolo {
+    Graph G;              // výstup polohy kola
+    Integrator v,y;       // stav systému kola
+public:
+    Kolo(int F, double M, double D, double k):
+    G("Výchylka", y, 0.01),
+    v((F-D*v-k*y)/M),
+    y(v) {}
+};
+sryrg
+Kolo k1(F, 2, 5.656, 400);
+
+int main() {                  // popis experimentu
+    Print(" Model tlumení kola automobilu v C++ \n");
+    OpenOutputFile("kolo.out");
+    Init(0,1.5);                // inicializace parametrů experimentu
+    SetStep(1e-3,0.1);          // krok integrace
+    SetAccuracy(0.001);         // max. povolená rel. chyba integrace
+    Run();                      // simulace
+    Print(" Konec simulace \n");
     return 0;
 }
+
 
