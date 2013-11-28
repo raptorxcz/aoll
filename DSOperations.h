6005e2c0948e959650df10aea3bea8dafc59fcbc
Your Name Here <your_email@example.com>
2013-11-28 19:58:05 +0100

    Rozdělení do souborů

---
+++ DSOperations.h
@@ -0,0 +1,162 @@
+//
+//  DSOperations.h
+//  ims
+//
+//  Created by Kryštof Matěj on 28.11.13.
+//  Copyright (c) 2013 Kryštof Matěj. All rights reserved.
+//
+
+#ifndef ims_DSOperations_h
+#define ims_DSOperations_h
+
+
+//! Scitani
+DSEquation operator+(DSBlock &v1, DSBlock &v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = &v2;
+    DSPlusBlock *p = new DSPlusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator+(DSEquation v1, DSBlock &v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = &v2;
+    DSPlusBlock *p = new DSPlusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator+(DSBlock &v1, DSEquation v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = v2.getResult();
+    DSPlusBlock *p = new DSPlusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator+(DSEquation v1, DSEquation v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = v2.getResult();
+    DSPlusBlock *p = new DSPlusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+//! odcitani
+DSEquation operator-(DSBlock &v1, DSBlock &v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = &v2;
+    DSMinusBlock *p = new DSMinusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator-(DSEquation v1, DSBlock &v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = &v2;
+    DSMinusBlock *p = new DSMinusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator-(DSBlock &v1, DSEquation v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = v2.getResult();
+    DSMinusBlock *p = new DSMinusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator-(DSEquation v1, DSEquation v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = v2.getResult();
+    DSMinusBlock *p = new DSMinusBlock(p1, p2);
+    return DSEquation(p);
+}
+
+
+//! nasobeni
+DSEquation operator*(DSBlock &v1, DSBlock &v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = &v2;
+    DSMultiBlock *p = new DSMultiBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator*(DSBlock &v1, DSEquation v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = v2.getResult();
+    DSMultiBlock *p = new DSMultiBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator*(DSEquation v1, DSBlock &v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = &v2;
+    DSMultiBlock *p = new DSMultiBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator*(DSEquation v1, DSEquation v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = v2.getResult();
+    DSMultiBlock *p = new DSMultiBlock(p1, p2);
+    return DSEquation(p);
+}
+
+//! deleni
+DSEquation operator/(DSBlock &v1, DSBlock &v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = &v2;
+    DSDivideBlock *p = new DSDivideBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator/(DSBlock &v1, DSEquation v2)
+{
+    DSBlock *p1 = &v1;
+    DSBlock *p2 = v2.getResult();
+    DSDivideBlock *p = new DSDivideBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator/(DSEquation v1, DSBlock &v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = &v2;
+    DSDivideBlock *p = new DSDivideBlock(p1, p2);
+    return DSEquation(p);
+}
+
+DSEquation operator/(DSEquation v1, DSEquation v2)
+{
+    DSBlock *p1 = v1.getResult();
+    DSBlock *p2 = v2.getResult();
+    DSDivideBlock *p = new DSDivideBlock(p1, p2);
+    return DSEquation(p);
+}
+
+//! unarni minus
+DSEquation operator-(DSBlock &v1)
+{
+    DSBlock *p1 = &v1;
+    DSUnaryMinusBlock *p = new DSUnaryMinusBlock(p1);
+    return DSEquation(p);
+}
+
+DSEquation operator-(DSEquation v1)
+{
+    DSBlock *p1 = v1.getResult();
+    DSUnaryMinusBlock *p = new DSUnaryMinusBlock(p1);
+    return DSEquation(p);
+}
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -8,34 +8,6 @@
 
 #include "DS.h"
 
-//#include "simlib/src/simlib.h"
-//
-//const double F = 1.0;
-//
-//class Kolo {
-////    Graph G;              // výstup polohy kola
-//    Integrator v,y;       // stav systému kola
-//public:
-//    Kolo(Input F, double M, double D, double k):
-////    G("Výchylka", y, 0.01){},
-//    v((F-D*v-k*y)/M),
-//    y(v) {}
-//};
-//
-//Kolo k1(F, 2, 5.656, 400);
-//
-//int main() {                  // popis experimentu
-//    Print(" Model tlumení kola automobilu v C++ \n");
-////    OpenOutputFile("kolo.out");
-//    Init(0,1.5);                // inicializace parametrů experimentu
-//    SetStep(1e-3,0.1);          // krok integrace
-//    SetAccuracy(0.001);         // max. povolená rel. chyba integrace
-//    Run();                      // simulace
-//    Print(" Konec simulace \n");
-//    return 0;
-//}
-
-
 int main()
 {
     DSParameterBlock op1(5), op2(3), op3(1), op4(4);
