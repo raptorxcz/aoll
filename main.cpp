16a7b0553f71d16951e55c07a0d661d6b674d081
Your Name Here <your_email@example.com>
2013-11-29 12:41:04 +0100

    - Přidáno zadání.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,16 @@
 #include "DS.h"
 
 struct Lorenz {
-    DSIntegratorBlock x1;
-    DSParameterBlock p1, p2;
-    Lorenz(double sigma, double lambda) :
-    p1(sigma) , p2(lambda) ,
-    x1(p1*(p2 - x1), 1){};           // dx1/dt = sigma * (x2 - x1)
+    DSIntegratorBlock x1, x2, x3;
+    DSParameterBlock p1, p2, p3, p4;
+    Lorenz(double sigma, double lambda, double b) :
+    p1(sigma), p2(lambda), p3(b), p4(1),
+    x1(p1*(x2 - x1), 1),           // dx1/dt = sigma * (x2 - x1)
+    x2((p4 + p2 - x3)*x1 - x2, 1), // dx2/dt = (1 + lambda - x3) * x1 - x2
+    x3(x1*x2 - p3*x3, 1){};          // dx1/dt = sigma * (x2 - x1)
 };
 
-Lorenz L(10, 2);
+Lorenz L(10, 24, 2);
 
 void Sample() {
 //    std::cout  <<  t.value() << " " << L.x1.value() << std::endl;
