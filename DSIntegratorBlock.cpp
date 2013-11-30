0fe834b16bc8431beea49a7c886ad1404a4300cc
Your Name Here <your_email@example.com>
2013-11-30 16:58:58 +0100

    -timer

--- DSIntegratorBlock.cpp
+++ DSIntegratorBlock.cpp
@@ -12,18 +12,14 @@ intType integratorType = EULER;
 
 double DSIntegratorBlock::value()
 {
-    if(t.value() <= currTime) // <=
-    {
+    if(t.value() <= currTime)
         return parametr;
-    }
     else
     {
         run();
         return parametr;
     }
 }
-
-
 
 DSIntegratorBlock::DSIntegratorBlock(DSEquation block, double value)
 {
